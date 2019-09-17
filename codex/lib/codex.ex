9b7cb4312e4ceb4b734474694a21a3a64cd077b5
alejandrito2318 <poncealejandro07@gmail.com>
2019-09-17 12:35:50 -0500

    Update codex.ex

--- codex/lib/codex.ex
+++ codex/lib/codex.ex
@@ -1,25 +1,51 @@
-defmodule CODEX do
+defmodule Principal do
   @moduledoc """
   Documentation for CODEX.
   """
-
   @commands %{
-      "help" => "Prints this help",
-      "a" =>"Another option"
+      "help" => "Ayuda",
+      "a" =>"imprime lista tokens",
+      "s" => "genera arbol",
+      "e" => "genera codigo ensamblador"
   }
 
 
     def help do
-      IO.puts("\CODEX --help file_name \n")
+      IO.puts("\CODEX Ayuda \n")
 
-      IO.puts("\nThe compiler supports following options:\n")
+      IO.puts("\nOpciones del compilador:\n")
 
       @commands
       |> Enum.map(fn {command, description} -> IO.puts("  #{command} - #{description}") end)
     end 
 
+    def a do
+      IO.puts("\LISTA DE TOKENS \n")
+      lista=Codex.lexear(File.read!("valueZero.c"))
+    end
+    def s do
+      IO.puts("\Genera Arbol\n")
+      lista=Codex.lexear(File.read!("valueZero.c"))
+      arbol=Parser.parse_program(lista)
+    end
+
+    def e do
+      IO.puts("\Genera codigo ensamblador\n")
+      IO.puts("\Genera Arbol\n")
+      lista=Codex.lexear(File.read!("valueZero.c"))
+      arbol=Parser.parse_program(lista)
+      GENERADOR.generate_code(arbol)
+    end
     defp process_args({[help: true], _, _}) do
       help()
     end
-  
+    defp process_args({[a: true], _, _}) do
+      a()
+    end
+     defp process_args({[s: true], _, _}) do
+      s()
+    end
+    defp process_args({[e: true], _, _}) do
+      e()
+    end
 end
