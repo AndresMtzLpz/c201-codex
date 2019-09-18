64ef51a00998e254c560c1caeffcbc82102ea702
alejandrito2318 <poncealejandro07@gmail.com>
2019-09-18 04:03:50 -0500

    1.2

--- codex/lib/codex.ex
+++ codex/lib/codex.ex
@@ -1,50 +1,60 @@
-defmodule Principal do
+defmodule CODEX do
   @moduledoc """
   Documentation for CODEX.
   """
   @commands %{
-      "help" => "Ayuda",
-      "a" =>"imprime lista tokens",
-      "s" => "genera arbol",
-      "e" => "genera codigo ensamblador"
+      "h" => "Muestra ayuda para la ejecución del compilador",
+      "t" =>"Muestra lista tokens",
+      "a" => "Genera arbol AST",
+      "s" => "Genera el codigo fuente del ensamblador",
+      "o" => "Especifica el nombre del ejecutable de nuestro compilador"
   }
+    def main (args) do
 
-
-    def help do
-      IO.puts("\CODEX Ayuda \n")
-
-      IO.puts("\nOpciones del compilador:\n")
-
-      @commands
-      |> Enum.map(fn {command, description} -> IO.puts("  #{command} - #{description}") end)
+     case args do
+      ["-h"] -> help() |> IO.puts();
+      ["-t"] -> t(); 
+      ["-a"] -> a();
+      ["-s"] -> s();
+      _ -> show_error(1) |> IO.puts;
+           show_error(1)
     end 
-
-    def a do
-      IO.puts("\LISTA DE TOKENS \n")
-      lista=Codex.lexear(File.read!("valueZero.c"))
+  end
+    def help() do
+    IO.puts("\CODEX Ayuda")
+    IO.puts("\nOpciones del compilador:\n")
+    "
+    -t      Muestra en pantalla la lista de tokens.
+    -a      Muestra el Árbol Sintáctico Abstracto.
+    -s      Genera el código fuente del ensamblador (x86).
+    "
     end
-    def s do
+
+    def t() do
+      #IO.puts("\LISTA DE TOKENS \n")
+      lista=Lexer.lexear(File.read!("multi_digit.c"))
+      IO.inspect(lista)
+    end
+
+    def a() do
       IO.puts("\Genera Arbol\n")
-      lista=Codex.lexear(File.read!("valueZero.c"))
+      lista=Lexer.lexear(File.read!("multi_digit.c"))
       arbol=Parser.parse_program(lista)
+      IO.inspect(arbol)
     end
 
-    def e do
+    def s() do
       IO.puts("\Genera codigo ensamblador\n")
-      lista=Codex.lexear(File.read!("valueZero.c"))
+      IO.puts("\Genera Arbol\n")
+      lista=Lexer.lexear(File.read!("multi_digit.c"))
       arbol=Parser.parse_program(lista)
       GENERADOR.generate_code(arbol)
     end
-    defp process_args({[help: true], _, _}) do
-      help()
-    end
-    defp process_args({[a: true], _, _}) do
-      a()
-    end
-     defp process_args({[s: true], _, _}) do
-      s()
-    end
-    defp process_args({[e: true], _, _}) do
-      e()
+    def show_error(num) do
+      case num do
+       1 -> "Compilador de C de Codex. Escriba -h para la ayuda." #no se puso argumento
+       2 -> "Comando(s) no válido. Escriba -h para la ayuda." #mensaje de error
+       3 -> "Archivo inválido o no existe en el directorio." #mensaje de archivo inválido
+      end
     end
 end
