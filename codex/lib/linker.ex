67efd1988e7c50266b15840a0b9dbe40e45c6db4
Andrés <46455749+AndresMtzLpz@users.noreply.github.com>
2019-09-19 10:36:33 -0500

    Linker 1.1

--- codex/lib/linker.ex
+++ codex/lib/linker.ex
@@ -2,16 +2,15 @@ defmodule Linker do
   def generate_binary(assembler, assembly_path) do
 	assembly_path = String.replace_trailing(assembly_path, ".c", ".s")
     assembly_file_name = Path.basename(assembly_path)
-	IO.inspect(assembly_file_name)
+	
     binary_file_name = Path.basename(assembly_path, ".s")
-	IO.inspect(binary_file_name)
     output_dir_name = Path.dirname(assembly_path)
-	IO.inspect(output_dir_name)
     assembly_path = output_dir_name <> "/" <> assembly_file_name
-	IO.inspect(assembly_path)
-
+	
     File.write!(assembly_path, assembler)
     System.cmd("gcc", [binary_file_name<>".c", "-o#{binary_file_name}"], cd: output_dir_name)
+	IO.puts("Archivo ejecutable generado -> #{output_dir_name}" <> "/" <> "#{binary_file_name}")
+	IO.puts("Archivo ensamblado generado -> #{assembly_path}")
 
    #File.rm!(assembly_path)
   end
