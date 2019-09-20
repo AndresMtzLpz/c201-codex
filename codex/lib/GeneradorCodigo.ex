d855a120f38fbb45d920130cc0f04c2473ce2407
alejandrito2318 <poncealejandro07@gmail.com>
2019-09-19 23:15:58 -0500

    generador ultimo

--- codex/lib/GeneradorCodigo.ex
+++ codex/lib/GeneradorCodigo.ex
@@ -1,38 +1,44 @@
 defmodule GENERADOR do
-def generador_codigo(arbol) do
-   	code = post_orden(arbol)
-	IO.puts("\nSalida del Generador de Codigo:")
-	IO.puts(code)
-   end 
-   def post_orden(nodo) do
-	if(nodo == nil) do
-	    nil
-	else
-	    encode = post_orden(nodo.hijoIzq)
-	    asemble(nodo.nodopadre, nodo.valor, encode)	 
-	end
+  def generate_code(arbol) do
+	code = post_order(arbol)
+  end
+
+  def post_order(node) do
+	#IO.inspect(node)
+    case node do
+  	nil ->
+        nil
+
+      anode ->
+        code_snippet = post_order(anode.hijoIzq)
+        emit_code(anode.nodopadre, code_snippet, anode.valor)
     end
-    def asemble(:constant, valor, _encode) do
-	"$#{valor}"
-    end
-    def asemble(:return, valor, encode) do
-	"""
-            movl    #{encode}, %eax
-            ret
-	""" 
-    end
-    def asemble(:function, valor, encode) do
-        """
-            .globl  _main         ## -- Begin function main
-        _main:                    ## @main
-    	""" <>
-      	encode
-    end
-    def asemble(:program, valor, encode) do
-	"""
-            .section        __TEXT,__text,regular,pure_instructions
-            .p2align        4, 0x90
-        """ <>
-	encode
-    end
+  end
+def emit_code(:program, code_snippet, _) do
+    """
+        .section        __TEXT,__text,regular,pure_instructions
+        .p2align        4, 0x90
+    """ <>
+	
+      code_snippet
+  end
+
+  def emit_code(:funcion, code_snippet, :main) do
+    """
+        .globl  _main         ## -- Begin function main
+    _main:                    ## @main
+    """ <>
+      code_snippet
+  end
+
+  def emit_code(:statement, code_snippet, :return) do
+    """
+        movl    #{code_snippet}, %eax
+        ret
+    """
+  end
+
+  def emit_code(:constant, _code_snippet, valor) do
+    "$#{valor}"
+end
 end
